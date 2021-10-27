a4db59bc6268fd4e24195904d2b4f6269e1bd8e0
vig29 <vig29@hi.is>
2021-10-27 19:57:18 +0000

    first commit First commit

--- scripts.js
+++ scripts.js
@@ -18,14 +18,21 @@ let losses = 0;
  * @return {boolean} true eða false
  */
 function isValidBestOf(bestOf) {
-  // TODO útfæra
+  if(bestOf>9) {return false}
+  else{
+    return (bestOf%2!==0)
+  }
+
 }
 // console.assert(isValidBestOf(1) === true, '1 er valid best of');
 // console.assert(isValidBestOf(2) === false, '2 er ekki er valid best of');
 // console.assert(isValidBestOf(9) === true, '9 er valid best of');
 
 function playAsText(play) {
-  // TODO útfæra
+  if(play===1){return 'Skæri'}
+  else if(play ===2){return 'Blað'}
+  else if(play ===3){return 'Steinn'}
+  else{return 'Óþekkt'}
 }
 // console.assert(playAsText('1') === 'Skæri', '1 táknar skæri');
 // console.assert(playAsText('2') === 'Blað', '2 táknar blað');
@@ -39,7 +46,10 @@ function playAsText(play) {
  * @returns -1 ef tölva vann, 0 ef jafntefli, 1 ef spilari vann
  */
 function checkGame(player, computer) {
-  // TODO útfæra
+  if(player === computer){return 0}
+  else if(player+1===computer){return 1}
+  else if(player-2===computer){return 1}
+  else{return -1}
 }
 // console.assert(checkGame('1', '2') === 1, 'Skæri vinnur blað');
 // console.assert(checkGame('2', '3') === 1, 'Blað vinnur stein');
@@ -52,32 +62,82 @@ function checkGame(player, computer) {
  * @return {boolean} -1 ef tölva vann, 0 ef jafntefli, 1 ef spilari vann
  */
 function round() {
-  // TODO útfæra
-  // 1. Spyrja um hvað spilað, ef cancel, hætta
-  // 2. Ef ógilt, tölva vinnur
-  // 3. Velja gildi fyrir tölvu með `Math.floor(Math.random() * 3) + 1` sem skilar heiltölu á [1, 3]
-  // 4. Nota `checkGame()` til að finna hver vann
-  // 5. Birta hver vann
-  // 6. Skila hver vann
+
+  let resround; //nidurstada umferdar
+  let computer = Math.floor(Math.random() * 3) + 1;
+
+  let valid = prompt('Veldur 1 fyrir skæri\nVeldu 2 fyrir blað\nVeldu 3 fyrir stein');
+
+  if(valid==null){
+    console.error('Hætt við leik');
+  }else if(playAsText(Number.parseInt(valid))=='Óþekkt'){
+    alert('Þú valdir: ' + playAsText(Number.parseInt(valid))+' sem er óþekkt' +'\nTölvan valdi: '+playAsText(computer) +'\nTölvan vann!')
+    return -1;
+  }else{
+    resround = checkGame(Number.parseInt(valid),computer);
+    if(resround==0){
+      alert('Þú valdir: ' + playAsText(Number.parseInt(valid)) +'\nTölvan valdi: '+playAsText(computer) +'\nJafntefli!')
+      return round();
+    }else if(resround==1){
+      alert('Þú valdir: ' + playAsText(Number.parseInt(valid)) +'\nTölvan valdi: '+playAsText(computer) +'\nÞú vannst umferð!\n' + playAsText(Number.parseInt(valid)) + ' vinnur ' +playAsText(computer))
+      return resround;
+    } else{
+      alert('Þú valdir: ' + playAsText(Number.parseInt(valid)) +'\nTölvan valdi: '+playAsText(computer) +'\nÞú tapðir umferð!\n' + playAsText(computer) + ' vinnur ' +playAsText(Number.parseInt(valid)))
+      return resround;
+    }
+  }
 }
-// Hér getum við ekki skrifað test þar sem fallið mun biðja notanda um inntak!
 
 /**
  * Spilar leik og bætir útkomu (sigur eða tap) við í viðeigandi global breytu.
  */
 function play() {
-  // TODO útfæra
-  // 1. Spyrja um fjölda leikja
-  // 2. Staðfesta að fjöldi leikja sé gilt gildi
-  // 3. Keyra fjölda leikja og spila umferð þar til sigurvegari er krýndur
-  // 4. Birta hvort spilari eða tölva vann
+  let leikir = prompt('Hversu marga leiki af best of í skæri, blað og steinn viltu spila.');
+
+  if(!isValidBestOf(Number.parseInt(leikir))){
+    alert('Ekki leyfilegur fjöldi leikja.')
+  }else if(leikir===null){
+    console.error('Hætt við leik')
+  }else{
+    let playedRounds=0;
+    let resround=0;
+    let playerWins=0;
+    let computerWins=0;
+    let winsTowin=Math.round(Number.parseInt(leikir)/2);
+
+    while(playedRounds<Number.parseInt(leikir)){
+      
+      playedRounds++;
+      resround=round();
+      
+      if(resround>0){
+        playerWins++;
+      }else{
+        computerWins++;
+      }
+
+      if(playerWins>winsTowin-1){
+        alert('Þú ert sigurvegari!');
+        wins++;
+        break;
+      }
+      if(computerWins>winsTowin-1){
+        alert('Þú tapaðir fyrir tölvunni');
+        losses++;
+        break;
+      }
+    }
+  }
 }
-// Hér getum við ekki skrifað test þar sem fallið mun biðja notanda um inntak!
+
 
 /**
  * Birtir stöðu spilara.
  */
 function games() {
-  // TODO útfæra
+  if(wins+losses===0){
+    console.log('Þú hefur spilað 0 leiki');
+  }else{
+    console.log('Þú hefur spilað '+(wins+losses)+' leiki. \nÞú hefur unnið '+wins+', eða ' + (100*wins/(wins+losses)).toFixed(2) +'% af heild'+'\nÞú hefur tapað '+losses+', eða ' + (100*losses/(wins+losses)).toFixed(2) +'% af heild');
+  }
 }
-// Hér getum við ekki skrifað test þar sem fallið les úr global state
